8dec5fed55264be16f834c0a49af10915f5fee63
EneaSema <sema.enea1993@gmail.com>
2025-04-14 17:47:19 +0200

    resolve-with-help-saint-donato

--- Snack1/javascript/snack1.js
+++ Snack1/javascript/snack1.js
@@ -1,11 +1,7 @@
 // console.log("ciao");
 
 // Creare un array di oggetti:
-let brandBike;
-let weightBike;
-console.log(brandBike);
-console.log(weightBike);
-
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 const bikes = [
   {
     brandBike: `Super`,
@@ -19,9 +15,19 @@ const bikes = [
     brandBike: `Ultra`,
     weightBike: 3,
   },
+  {
+    brandBike: `X-Turbo`,
+    weightBike: 9,
+  },
 ];
 
-console.log(bikes[2].brandBike);
-
-// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // Stampare a schermo la bici con peso minore.
+let result = bikes[0];
+console.log(result.weightBike);
+
+for (let i = 0; i < bikes.length; i++) {
+  if (bikes[i].weightBike < result.weightBike) {
+    result = bikes[i];
+  }
+}
+console.log(result);
