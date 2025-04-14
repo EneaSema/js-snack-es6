47887caa34f80059bc14b02a3323b9773fc6101e
EneaSema <sema.enea1993@gmail.com>
2025-04-14 17:05:56 +0200

    developer-exercise-one

---
+++ Snack1/javascript/snack1.js
@@ -0,0 +1,27 @@
+// console.log("ciao");
+
+// Creare un array di oggetti:
+let brandBike;
+let weightBike;
+console.log(brandBike);
+console.log(weightBike);
+
+const bikes = [
+  {
+    brandBike: `Super`,
+    weightBike: 7,
+  },
+  {
+    brandBike: `Turbo`,
+    weightBike: 5,
+  },
+  {
+    brandBike: `Ultra`,
+    weightBike: 3,
+  },
+];
+
+console.log(bikes[2].brandBike);
+
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore.
